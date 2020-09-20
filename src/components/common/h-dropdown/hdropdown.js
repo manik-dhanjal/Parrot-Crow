2fbe3ffe9cc0cf46d561fd28ce84490eee1ed085
manik21 <manikdhanjal217@gmail.com>
2020-09-20 19:46:09 +0530

    links updated

--- src/components/common/h-dropdown/hdropdown.js
+++ src/components/common/h-dropdown/hdropdown.js
@@ -19,32 +19,32 @@ const DropDown=(props)=>{
                                    
                                         <ul className="boy-list cat-list">
                                             <li>
-                                                <a href="/collections/boys-t-shirts">
+                                                <a href="https://www.parrotcrow.in/collections/boys-t-shirts">
                                                     T- Shirts
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="/collections/boys-polos">
+                                                <a href="https://www.parrotcrow.in/collections/boys-polos">
                                                     Polos
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="/collections/shorts">
+                                                <a href="https://www.parrotcrow.in/collections/boys-t-shirts">
                                                     Shorts
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="/collections/jeans">
+                                                <a href="https://www.parrotcrow.in/collections/jeans">
                                                     Jeans
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="/collections/jogger">
+                                                <a href="https://www.parrotcrow.in/collections/jogger">
                                                     Joggers
                                                 </a>
                                             </li>
                                         </ul>
-                                        <div className="cat-hover"> <a href="#" className="boys-button cat-button">Shop Boys <FontAwesomeIcon icon={faAngleDoubleRight} /></a></div>
+                                        <div className="cat-hover"> <a href="https://www.parrotcrow.in/collections/boys" className="boys-button cat-button">Shop Boys <FontAwesomeIcon icon={faAngleDoubleRight} /></a></div>
                                     </div>
                                 </div>
                                 <div
@@ -57,22 +57,22 @@ const DropDown=(props)=>{
                                         <h2 className="drop-head">Girls</h2>
                                         <ul className="girl-list cat-list">
                                             <li>
-                                                <a href="/collections/girls-t-shirts/retail">
+                                                <a href="https://www.parrotcrow.in/collections/girls-t-shirts/retail">
                                                     T-Shirts
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="/collections/girls-capri/retail">
+                                                <a href="https://www.parrotcrow.in/collections/girls-capri/retail">
                                                     Capri
                                                 </a>
                                             </li>
-                                            <li>
+                                            {/* <li>
                                                 <a href="/collections/shorts-1/retail">
                                                     Shorts
                                                 </a>
-                                            </li>
+                                            </li> */}
                                         </ul>
-                                            <div className="cat-hover"> <a className="girls-button cat-button" href="#">Shop Girls <FontAwesomeIcon icon={faAngleDoubleRight} /></a></div>
+                                            <div className="cat-hover"> <a className="girls-button cat-button" href="https://www.parrotcrow.in/collections/girls">Shop Girls <FontAwesomeIcon icon={faAngleDoubleRight} /></a></div>
                                         </div >
                                 </div>
                             </div>
